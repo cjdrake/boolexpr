b7010380e5dfa6367eaa5679f3e6e0fe43711cbf
Chris Drake <cjdrake@gmail.com>
2016-07-05 17:16:33 -0700

    Update Tseytin coverage

--- test/tseytin_test.cc
+++ test/tseytin_test.cc
@@ -50,20 +50,20 @@ TEST_F(TseytinTest, Operators)
 {
     auto ctx = Context();
 
-    auto y0 = nor_s( {xs[0], xor_s( {xs[1], xs[2]}), xs[3]});
-    auto y1 = or_s(  {xs[0], xnor_s({xs[1], xs[2]}), xs[3]});
-    auto y2 = nand_s({xs[0], or_s(  {xs[1], xs[2]}), xs[3]});
-    auto y3 = and_s( {xs[0], nor_s( {xs[1], xs[2]}), xs[3]});
-    auto y4 = xnor_s({xs[0], and_s( {xs[1], xs[2]}), xs[3]});
-    auto y5 = xor_s( {xs[0], nand_s({xs[1], xs[2]}), xs[3]});
-    auto y6 = neq_s( {xs[0], eq_s(  {xs[1], xs[2]}), xs[3]});
-    auto y7 = eq_s(  {xs[0], neq_s( {xs[1], xs[2]}), xs[3]});
+    auto y0 =  nor_s({xs[0],  xor_s({xs[1], xs[2]}), xs[3]});
+    auto y1 =   or_s({xs[0], xnor_s({xs[1], xs[2]}), xs[3]});
+    auto y2 = nand_s({xs[0],   or_s({xs[1], xs[2]}), xs[3]});
+    auto y3 =  and_s({xs[0],  nor_s({xs[1], xs[2]}), xs[3]});
+    auto y4 = xnor_s({xs[0],  and_s({xs[1], xs[2]}), xs[3]});
+    auto y5 =  xor_s({xs[0], nand_s({xs[1], xs[2]}), xs[3]});
+    auto y6 =  neq_s({xs[0],   eq_s({xs[1], xs[2]}), xs[3]});
+    auto y7 =   eq_s({xs[0],  neq_s({xs[1], xs[2]}), xs[3]});
 
-    auto y8 = nimpl_s(xs[0], or_s( {xs[1], xs[2]}));
-    auto y9 = impl_s( xs[0], nor_s({xs[1], xs[2]}));
+    auto y8 = nimpl_s(xs[0],  or_s({xs[1], xs[2]}));
+    auto y9 = impl_s (xs[0], nor_s({xs[1], xs[2]}));
 
-    auto y10 = nite_s(xs[0], xs[1], and_s( {xs[2], xs[3]}));
-    auto y11 = ite_s( xs[0], xs[1], nand_s({xs[2], xs[3]}));
+    auto y10 = nite_s(xs[0], xs[1],  and_s({xs[2], xs[3]}) );
+    auto y11 =  ite_s(xs[0], xs[1], nand_s({xs[2], xs[3]}));
 
     EXPECT_TRUE(y0->tseytin(ctx)->is_cnf());
     EXPECT_TRUE(y1->tseytin(ctx)->is_cnf());
@@ -77,4 +77,16 @@ TEST_F(TseytinTest, Operators)
     EXPECT_TRUE(y9->tseytin(ctx)->is_cnf());
     EXPECT_TRUE(y10->tseytin(ctx)->is_cnf());
     EXPECT_TRUE(y11->tseytin(ctx)->is_cnf());
+}
+
+
+TEST_F(TseytinTest, CNF)
+{
+    auto ctx = Context();
+
+    auto y0 = onehot({xs[0], xs[1], xs[2], xs[3]});
+    auto y1 = y0->tseytin(ctx);
+
+    EXPECT_TRUE(y0->is_cnf());
+    EXPECT_EQ(y0->size(), y1->size());
 }
